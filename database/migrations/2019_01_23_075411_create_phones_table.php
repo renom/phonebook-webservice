f37699d1c23f15f9d661f26b01d3b6e220662c0f
ReNoM <renom@list.ru>
2019-01-23 11:43:17 +0300

    Database migrations: contacts, phones

---
+++ database/migrations/2019_01_23_075411_create_phones_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('contact_id');
+            $table->string('number', 45);
+            $table->timestamps();
+            
+            $table->foreign('contact_id')->references('id')->on('contacts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
