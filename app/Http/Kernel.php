00a5468b44cdd92b8169783fd21fceba36fecc71
ReNoM <renom@list.ru>
2019-01-24 12:43:39 +0300

    Disable the web middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middlewareGroups = [
-        'web' => [
+        /*'web' => [
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -35,7 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+        ],*/
 
         'api' => [
             'throttle:60,1',
